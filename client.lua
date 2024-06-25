e697e4b213fbb52feb2fabfa85b58488559cc7b1
f4st3r <152923524+f4st3rxd@users.noreply.github.com>
2024-06-25 13:36:19 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,19 @@
+CreateThread(function()
+    local sleep = 1250
+    while true do 
+        Wait(sleep)
+
+        if IsPedInAnyVehicle(PlayerPedId(), false) then 
+            sleep = 1
+        else
+            sleep = 1250
+        end 
+        
+        local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+        if GetVehicleClass(vehicle) == 8 then 
+            if IsControlPressed(0, 73) or IsControlPressed(0, 25) or IsControlPressed(0, 24)  then
+                ClearPedTasks(PlayerPedId())
+            end
+        end
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'f4st3r'
+
+client_scripts {"client.lua"}
