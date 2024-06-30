4e9046037210842052b176338895020dcb4d5adf
f4st3r <152923524+f4st3rxd@users.noreply.github.com>
2024-06-30 18:22:16 +0300

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,14 +1,14 @@
 CreateThread(function()
-    local sleep = 1250
+    local sleep = 2250
     while true do 
         Wait(sleep)
 
-        if IsPedInAnyVehicle(PlayerPedId(), false) then 
+        if IsPedInAnyVehicle(PlayerPedId(), false) and GetVehicleClass(vehicle) == 8then 
             sleep = 1
         else
-            sleep = 1250
+            sleep = 2250
         end 
-        
+
         local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
         if GetVehicleClass(vehicle) == 8 then 
             if not GetSelectedPedWeapon(PlayerPedId()) == GetHashKey("WEAPON_UNARMED") then return end
